d057933cbe09fc71cdd0357db3fa5d36c0cc7531
StefLaur62 <stephen.laurent62@gmail.com>
2020-12-18 17:03:02 +0100

    Added Village NavMesh

---
+++ Assets/Scripts/Enemy/EnemyMoveToPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMoveToPlayer : MonoBehaviour
+{
+    public Transform goal;
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(goal.position);
+        agent.stoppingDistance = 2f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        agent.SetDestination(goal.position);
+    }
+}
---
+++ Assets/Scripts/Props/OpenGate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGate : MonoBehaviour
+{
+    [SerializeField]
+    private float speed=0.6f;
+
+    private Vector3 openPos;
+    private Vector3 closePos;
+
+    public Transform player;
+    void Start()
+    {
+        closePos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        openPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        openPos += new Vector3(0,2,0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float dist = Vector3.Distance(player.transform.position, this.transform.position);
+        if (dist <= 4f)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, openPos, step);
+        } 
+        else if (dist > 4f)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, closePos, step);
+        }
+    }
+}
