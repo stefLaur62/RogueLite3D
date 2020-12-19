1521dcf423a898c2452557f81908d29f54a0ff52
StefLaur62 <stephen.laurent62@gmail.com>
2020-12-19 20:43:06 +0100

    Updated FootSteps sound

--- Assets/Scripts/Player/PlayerFootSteps.cs
+++ Assets/Scripts/Player/PlayerFootSteps.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerFootSteps : MonoBehaviour
 {
     [SerializeField]
-    private AudioClip clip;
+    private AudioClip[] clips;
     private AudioSource audioSource;
+    private float volume = 1f;
+
 
     void Start()
     {
@@ -19,6 +21,36 @@ public class PlayerFootSteps : MonoBehaviour
     }
     private void Step()
     {
-        audioSource.PlayOneShot(clip);
+
+        audioSource.PlayOneShot(getClip(),volume);
+    }
+    private AudioClip getClip()
+    {
+        if (!isGrass())
+        {
+            volume = 1f;
+            return clips[0];
+        }
+        else
+        {
+            volume = 0.1f;
+            return clips[1];
+        }
+    }
+
+    private bool isGrass()
+    {
+        RaycastHit hit = new RaycastHit();
+        Debug.DrawRay(transform.position, Vector3.down);
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            Debug.Log(hit.collider.gameObject.tag);
+            if (hit.collider.gameObject.tag == "Grass")
+            {
+                return true;
+            }
+
+        }
+        return false;
     }
 }
