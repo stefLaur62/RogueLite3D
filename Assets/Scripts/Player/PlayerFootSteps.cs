31612f814106b56106c3f1cc7ed24a5dbe95951b
StefLaur62 <stephen.laurent62@gmail.com>
2020-12-19 17:31:33 +0100

    Added footsteps sound

---
+++ Assets/Scripts/Player/PlayerFootSteps.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootSteps : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip clip;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        
+    }
+    private void Step()
+    {
+        audioSource.PlayOneShot(clip);
+    }
+}
